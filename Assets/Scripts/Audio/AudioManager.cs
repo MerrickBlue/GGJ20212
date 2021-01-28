4c5b516622ab7470f8a89af441b1734c456ed318
Audiosource <Audiosource@1c247438-4568-4e6e-8272-7da0186db068>
2021-01-28 22:21:14 +0000

    Created Audio Manager functionality. Also created its prefab, ready to use. Added some TEMP audio files for testing. Adjusted mixer for audio manager functions

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -5,8 +5,22 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    private static AudioManager _audioManag;
+    [Header("Music")]
+    public AudioMixer MusicMixer;
 
+    public AudioSource MusicIntro; //these are childed gameobjects with audiosources and clips set. audiosources on loop (except Win and Death)
+    public AudioSource MusicGame;
+    public AudioSource MusicWin;
+    public AudioSource MusicDeath;
+
+
+    [Header("SFX")]
+    public AudioMixer SFXMixer;
+
+    public AudioSource GenAmbience; //same as music
+
+    //Singleton
+    private static AudioManager _audioManag;
     public static AudioManager AudioManag { get { return _audioManag; } }
 
     private void Awake()
@@ -23,14 +37,227 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    void Start()
+    #region SFX Control
+    public void PlaySFX(AudioSource sfxSource, AudioClip[] sfxClips, float minPitch, float maxPitch, float minVol, float maxVol, bool oneAtTime)
     {
-       
+        var clip = AudioRandom.RandomAudioClip(sfxClips.Length);
+
+        AudioRandom.RandomAudioPitch(sfxSource, minPitch, maxPitch);
+        sfxSource.volume = AudioRandom.RandomAudioVolume(minVol, maxVol);
+
+        if (oneAtTime)
+        {
+            if (!sfxSource.isPlaying)
+                sfxSource.PlayOneShot(sfxClips[clip]);
+        }
+        else
+        {
+            sfxSource.PlayOneShot(sfxClips[clip]);
+        }
     }
 
+    public void StopSFX(AudioSource sfxSource)
+    {
+        sfxSource.Stop();
+    }
+    #endregion
+
+    #region Music Control
+
+    public void StartMusic()
+    {
+        if (!MusicIntro.isPlaying && !MusicGame.isPlaying)
+        {
+            MusicIntro.Play();
+            MusicGame.Play();
+        }
+    }
+
+    public void FadeInIntro(float duration)
+    {
+        StartCoroutine(AudioFade.MixerFadeIn(MusicMixer, "IV", duration));
+        StartCoroutine(AudioFade.MixerFadeOut(MusicMixer, "GV", duration));
+    }
+
+    public void FadeInGame(float duration)
+    {
+        StartCoroutine(AudioFade.MixerFadeIn(MusicMixer, "GV", duration));
+    }
+
+    public void PlayWinMusic(float duration) //Should have good ducking on mixer
+    {
+        if (!MusicWin.isPlaying)
+        {
+            MusicWin.Play();
+            StartCoroutine(AudioFade.MixerFadeIn(MusicMixer, "WV", duration));
+            Invoke("FadeOutWinMus", MusicWin.clip.length + 0.2f);
+        }
+    }
+
+    void FadeOutWinMus()
+    {
+        StartCoroutine(AudioFade.MixerFadeOut(MusicMixer, "WV", 0.2f));
+    }
+
+    public void PlayDeathMusic(float duration) //Should have good ducking on mixer
+    {
+        if (!MusicDeath.isPlaying)
+        {
+            MusicDeath.Play();
+            StartCoroutine(AudioFade.MixerFadeIn(MusicMixer, "DV", duration));
+            Invoke("FadeOutDeathMus", MusicDeath.clip.length + 0.2f);
+        }
+    }
+
+    void FadeOutDeathMus()
+    {
+        StartCoroutine(AudioFade.MixerFadeOut(MusicMixer, "DV", 0.2f));
+    }
+
+    public void PauseMus()
+    {
+        MusicIntro.Pause();
+        MusicGame.Pause();
+        MusicWin.Pause();
+        MusicDeath.Pause();
+    }
+
+    public void UnPauseMus()
+    {
+        MusicIntro.UnPause();
+        MusicGame.UnPause();
+        MusicWin.UnPause();
+        MusicDeath.UnPause();
+    }
+
+    public void StopMus()
+    {
+        MusicIntro.Stop();
+        MusicGame.Stop();
+        MusicWin.Stop();
+        MusicDeath.Stop();
+    }
+
+    #endregion
+
+    #region Ambience Control
+
+    public void PlayAmbience(float duration)
+    {
+        if (!GenAmbience.isPlaying)
+        {
+            GenAmbience.Play();
+            StartCoroutine(AudioFade.MixerFadeIn(SFXMixer, "AV", duration));
+        }
+    }
+
+    public void StopAmbience(float duration)
+    {
+        if (GenAmbience.isPlaying)
+        {
+            StartCoroutine(AudioFade.MixerFadeOut(SFXMixer, "AV", duration));
+            Invoke("StoppingAmbience", duration + 0.1f);
+        }
+    }
+
+    void StoppingAmbience()
+    {
+        GenAmbience.Stop();
+    }
+    #endregion
+
     
+    #region Testing
+    /*
+    [Header("Test Tools")]
+    public bool PlayMusic;
+    public bool CueIntro;
+    public bool CueGame;
+    public bool CueWin;
+    public bool CueDeath;
+    public bool PauseM;
+    public bool UnPauseM;
+    public bool StopM;
+    public bool SFXtest;
+    public AudioClip[] TestSFX;
+    public AudioSource TestASource;
+    public bool OneTime;
+    public bool StopSound;
+    public bool CueAmb;
+    public bool StopAmb;
+
     void Update()
     {
-        
+        if (PlayMusic)
+        {
+            StartMusic();
+            PlayMusic = false;
+        }
+        if (CueIntro)
+        {
+            FadeInIntro(1f);
+            CueIntro = false;
+        }
+        if (CueGame)
+        {
+            FadeInGame(1f);
+            CueGame = false;
+        }
+        if (CueWin)
+        {
+            PlayWinMusic(1f);
+            CueWin = false;
+        }
+        if (CueDeath)
+        {
+            PlayDeathMusic(1f);
+            CueDeath = false;
+        }
+        if (PauseM)
+        {
+            PauseMus();
+            PauseM = false;
+        }
+        if (UnPauseM)
+        {
+            UnPauseMus();
+            UnPauseM = false;
+        }
+        if (StopM)
+        {
+            StopMus();
+            StopM = false;
+        }
+        if (SFXtest)
+        {
+            if (OneTime)
+            {
+                PlaySFX(TestASource, TestSFX, 0.7f, 1.3f, 0.8f, 1f, true);
+            }
+            else
+            {
+                PlaySFX(TestASource, TestSFX, 1f, 1f, 1f, 1f, false);
+            }
+
+            SFXtest = false;
+        }
+        if (StopSound)
+        {
+            StopSFX(TestASource);
+        }
+        if (CueAmb)
+        {
+            PlayAmbience(1f);
+            CueAmb = false;
+        }
+        if (StopAmb)
+        {
+            StopAmbience(1f);
+            StopAmb = false;
+        }
+
     }
+    */
+    #endregion
+    
 }
